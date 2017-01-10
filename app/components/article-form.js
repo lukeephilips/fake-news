67c51a341ca758d6813fd04f92e7498a7a64b8f7
Jim Padilla and Luke Philips <jim+luke@>
2017-01-10 14:36:26 -0800

    add save article

--- app/components/article-form.js
+++ app/components/article-form.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // didInsertElement() {
-  //   Ember.$('.input-field').
-  // }
+  addNewArticle: false,
+  actions: {
+    articleToggle() {
+      this.toggleProperty('addNewArticle');
+    },
+    saveArticle() {
+      var params = {
+        title: this.get('title') ? this.get('title'): "",
+        author: this.get('author') ? this.get('author'): "",
+        text: this.get('text') ? this.get('text'): "",
+        image: this.get('image') ? this.get('image'): "",
+      };
+      this.toggleProperty('addNewArticle');
+      this.sendAction('saveArticle',params);
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('article');
+  },
+
+  actions: {
+    saveArticle(params) {
+      var newArticle = this.store.createRecord('article', params);
+      newArticle.save();
+      this.transitionTo('admin');
+    },
+
+    destroyArticle(article) {
+      rental.destroyRecord();
+      this.transitionTo('admin');
+    }
+  }
 });
