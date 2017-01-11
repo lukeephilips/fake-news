38865afaaa9562690d507d6950140451a0952e40
Jim Padilla and Luke Philips <jim+luke@>
2017-01-10 16:37:03 -0800

    this is the worst

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  didInsertElement () {
+   Ember.$('.carousel').carousel();
+  },
   model() {
     return this.store.findAll('article');
   }
